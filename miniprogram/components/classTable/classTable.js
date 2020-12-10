eeb482fbc98b2faa34ff5309d028870d66a99f7d
chenjiaqiang-a <2504919775@qq.com>
2020-12-10 23:06:45 +0800

    classTable完成

--- miniprogram/components/classTable/classTable.js
+++ miniprogram/components/classTable/classTable.js
@@ -10,9 +10,6 @@ Component({
     onAdd: {
       type: Function
     },
-    onChange: {
-      type: Function
-    },
     onDelete: {
       type: Function
     }
@@ -23,55 +20,31 @@ Component({
    */
   data: {
     table: [],
-    weekDays: ['星期一','星期二','星期三','星期四','星期五','星期六','星期天'],
-    index: null,
-    picker: ['必修', '选修'],
-    multiArray: [
-      ['望江校区', '江安校区','华西校区'],
-      ['东二教', '东三教', '基教楼A座', '基教楼C座'],
-    ],
-    objectMultiArray: [
-      [{
-          id: 0,
-          name: '望江校区'
-        },
-        {
-          id: 1,
-          name: '江安校区'
-        },
-        {
-          id: 2,
-          name: '华西校区'
-        }
-      ],
-      [{
-          id: 0,
-          name: '东二教'
-        },
-        {
-          id: 1,
-          name: '东三教'
-        },
-        {
-          id: 2,
-          name: '基教楼A座'
-        },
-        {
-          id: 3,
-          name: '基教楼C座'
-        }
-      ],
-    ],
+    weekDays: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'],
+
     curCol: 0,
-    curRow: 0
+    curRow: 0,
+
+    className: "",
+    teacherName: "",
+    classroomName: "",
+    classTypeIndex: 0,
+    classType: ['必修', '选修'],
+    classLenIndex: 0,
+    classLen: [1, 2, 3, 4, 5, 6, 7, 8],
+    placeIndex: [0, 0],
+    placeArray: [
+      ['江安校区', '望江校区', '华西校区'],
+      ['一教A座', '一教B座', '一教C座', '一教D座', '综合楼B座', '综合楼C座', '文科楼一区', '文科楼二区', '文科楼三区', '二基楼', '一基楼']
+    ],
   },
- 
+
   lifetimes: {
     ready: function () {
       let table = []
       for (let c = 0; c < 7; c++) {
         let col = []
-        for (let r = 0; r < 12; r++ ) {
+        for (let r = 0; r < 12; r++) {
           let course = {
             hasCourse: false,
             isBeginning: false,
@@ -81,7 +54,6 @@ Component({
             teacherName: "",
             classType: "",
             location: "",
-            weekDescription: "",
             classLen: 0
           }
           col.push(course)
@@ -93,18 +65,18 @@ Component({
         for (let j = 0; j < course.timeAndPlaceList.length; j++) {
           let c = course.timeAndPlaceList[j].classDay
           let r = course.timeAndPlaceList[j].classSessions
-          table[c-1][r-1].hasCourse = true
-          table[c-1][r-1].isBeginning = true
-          table[c-1][r-1].classid = course.timeAndPlaceList[j].classid
-          table[c-1][r-1]._id = course._id
-          table[c-1][r-1].className = course.className
-          table[c-1][r-1].teacherName = course.teacherName
-          table[c-1][r-1].classType = course.classType
-          table[c-1][r-1].weekDescription = course.timeAndPlaceList[j].weekDescription
-          table[c-1][r-1].location = course.timeAndPlaceList[j].campusName + course.timeAndPlaceList[j].teachingBuildingName + course.timeAndPlaceList[j].classroomName
-          table[c-1][r-1].classLen = course.timeAndPlaceList[j].continuingSession
-          for (let k = 1; k < table[c-1][r-1].classLen; k++) {
-            table[c-1][r-1+k].hasCourse = true
+          table[c - 1][r - 1].hasCourse = true
+          table[c - 1][r - 1].isBeginning = true
+          table[c - 1][r - 1].classid = course.timeAndPlaceList[j].classid
+          table[c - 1][r - 1]._id = course._id
+          table[c - 1][r - 1].className = course.className
+          table[c - 1][r - 1].teacherName = course.teacherName
+          table[c - 1][r - 1].classType = course.classType
+          table[c - 1][r - 1].weekDescription = course.timeAndPlaceList[j].weekDescription
+          table[c - 1][r - 1].location = course.timeAndPlaceList[j].campusName + course.timeAndPlaceList[j].teachingBuildingName + course.timeAndPlaceList[j].classroomName
+          table[c - 1][r - 1].classLen = course.timeAndPlaceList[j].continuingSession
+          for (let k = 1; k < table[c - 1][r - 1].classLen; k++) {
+            table[c - 1][r - 1 + k].hasCourse = true
           }
         }
       }
@@ -119,33 +91,131 @@ Component({
    */
   methods: {
     showModal(e) {
-        console.log(e)
-        this.setData({
-          modalName: e.currentTarget.dataset.target,
-          curCol: e.currentTarget.dataset.curcol, 
-          curRow: e.currentTarget.dataset.currow
-        })
+      this.setData({
+        modalName: e.currentTarget.dataset.target,
+        curCol: e.currentTarget.dataset.curcol,
+        curRow: e.currentTarget.dataset.currow
+      })
     },
     hideModal(e) {
       this.setData({
         modalName: null
       })
     },
-    onAdd:function(){
-      setTimeout(() => {
-        this.hideModal()
 
+    onShowAdd: function () {
+      this.setData({
+        modalName: 'Add'
+      })
+    },
+
+    onClassNameChange: function (e) {
+      this.setData({
+        className: e.detail.value
+      })
+    },
+    onTeacherNameChange: function (e) {
+      this.setData({
+        teacherName: e.detail.value
+      })
+    },
+    onClassroomNameChange: function (e) {
+      this.setData({
+        classroomName: e.detail.value
+      })
+    },
+    onClassTypeChange: function (e) {
+      this.setData({
+        classTypeIndex: e.detail.value
+      })
+    },
+    onClassLenChange: function (e) {
+      this.setData({
+        classLenIndex: e.detail.value
+      })
+    },
+    onPlaceChange(e) {
+      this.setData({
+        multiIndex: e.detail.value
+      })
+    },
+    onCampusChange(e) {
+      let data = {
+        placeArray: this.data.placeArray,
+        placeIndex: this.data.placeIndex
+      };
+      data.placeIndex[e.detail.column] = e.detail.value
+      if (e.detail.column == 0) {
+        switch (data.placeIndex[0]) {
+          case 0:
+            data.placeArray[1] = ['一教A座', '一教B座', '一教C座', '一教D座', '综合楼B座', '综合楼C座', '文科楼一区', '文科楼二区', '文科楼三区']
+            break
+          case 1:
+            data.placeArray[1] = ['东二教', '东三教', '基教楼A座', '基教楼C座']
+            break
+          case 2:
+            data.placeArray[1] = ['九教', '十教']
+            break
+        }
+        data.placeIndex[1] = 0
+      }
+      this.setData(data);
+    },
+    onSubmit: function () {
+      this.setData({
+        loadModalAdd: true
+      })
+      const course = {
+        className: this.data.className,
+        teacherName: this.data.teacherName,
+        classType: this.data.classType[this.data.classTypeIndex],
+        timeAndPlaceList: [{
+          classid: 0,
+          classDay: this.data.curCol+1,
+          classSessions: this.data.curRow+1,
+          campusName: this.data.placeArray[0][this.data.placeIndex[0]],
+          teachingBuildingName: this.data.placeArray[1][this.data.placeIndex[1]],
+          classroomName: this.data.classroomName,
+          continuingSession: this.data.classLen[this.data.classLenIndex]
+        }]
+      }
+      this.properties.onAdd(course)
+      setTimeout(() => {
+        this.onReset()
         this.setData({
-          loadModalAdd: false,
-          hasContent: false
+          loadModalAdd: false
         })
       }, 500)
     },
-    onChange:function(){
+    onReset: function() {
+      this.hideModal()
+      this.setData({
+        className: "",
+        teacherName: "",
+        classroomName: "",
+        classTypeIndex: 0,
+        classLenIndex: 0,
+        placeIndex: [0, 0],
+        placeArray: [
+          ['江安校区', '望江校区', '华西校区'],
+          ['一教A座', '一教B座', '一教C座', '一教D座', '综合楼B座', '综合楼C座', '文科楼一区', '文科楼二区', '文科楼三区', '二基楼', '一基楼']
+        ]
+      })
+    },
 
+    onTapDelete: function() {
+      this.setData({
+        toDelete: true
+      })
+    },
+    notDelete: function() {
+      this.setData({
+        toDelete:false
+      })
     },
     onDelete: function () {
       this.setData({
+        toDelete: false,
         loadModalDel: true
       })
       const course = this.data.table[this.data.curCol][this.data.curRow]
@@ -157,55 +227,6 @@ Component({
         })
       }, 500)
     },
-    onClassChange: function (e) {
-      if (e.detail.value) {
-        this.setData({
-          className: e.detail.value,
-          timeAndPlaceList: e.detail.value,
-          
-        })
-      } else {
-        this.setData({
-          className: e.detail.value,
-          timeAndPlaceList: e.detail.value,
-          
-        })
-      }
-    },
-    PickerChange(e) {
-      console.log(e);
-      this.setData({
-        index: e.detail.value
-      })
-    },
-    MultiChange(e) {
-      this.setData({
-        multiIndex: e.detail.value
-      })
-    },
-    MultiColumnChange(e) {
-      let data = {
-        multiArray: this.data.multiArray,
-        multiIndex: this.data.multiIndex
-      };
-      data.multiIndex[e.detail.column] = e.detail.value;
-      switch (e.detail.column) {
-        case 0:
-          switch (data.multiIndex[0]) {
-            case 0:
-              data.multiArray[1] = ['东二教', '东三教', '基教楼A座', '基教楼C座'];
-              break
-            case 1:
-              data.multiArray[1] = ['一教A座', '一教B座', '一教C座', '一教D座', '综合楼B座', '综合楼C座', '文科楼一区', '文科楼二区', '文科楼三区'];
-              break
-            case 2:
-              data.multiArray[1] = ['九教', '十教'];
-              break;
-          }
-          data.multiIndex[1] = 0;
-          break;
-      }
-      this.setData(data);
-    },
+
   }
 })
--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -1,219 +1,217 @@
 // miniprogram/pages/main/main.js
-const table = [
-  {
-      _id: "0",
-      _openid: "10000100000",
-      className: "形势与政策-3",
-      teacherName: "刘辉",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classid:0,
-          classDay: 5,
-          classSessions: 8,
-          campusName: "江安",
-          teachingBuildingName: "综合楼C座",
-          classroomName: "C306",
-          weekDescription: "3-9周单",
-          continuingSession: 2
-      }]
+const table = [{
+  _id: "0",
+  _openid: "10000100000",
+  className: "形势与政策-3",
+  teacherName: "刘辉",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classid: 0,
+    classDay: 5,
+    classSessions: 8,
+    campusName: "江安",
+    teachingBuildingName: "综合楼C座",
+    classroomName: "C306",
+    weekDescription: "3-9周单",
+    continuingSession: 2
+  }]
+}, {
+  _id: "1",
+  _openid: "10000100000",
+  className: "马克思主义基本原理概论",
+  teacherName: "赵苏丹* 蒋永穆 ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 1,
+    classSessions: 3,
+    campusName: "江安",
+    teachingBuildingName: "综合楼C座",
+    classroomName: "C503",
+    weekDescription: "2-18周",
+    continuingSession: 2
+  }]
+}, {
+  _id: "2",
+  _openid: "10000100000",
+  className: "体育-3",
+  teacherName: "ty9* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 3,
+    classSessions: 8,
+    campusName: "江安",
+    teachingBuildingName: "体育场",
+    classroomName: "体育场2号",
+    weekDescription: "2-17周",
+    continuingSession: 2
+  }]
+}, {
+  _id: "3",
+  _openid: "10000100000",
+  className: "中华文化（文学篇）",
+  teacherName: "谢谦* ",
+  classType: "任选",
+  timeAndPlaceList: [{
+    classDay: 1,
+    classSessions: 5,
+    campusName: "江安",
+    teachingBuildingName: "一教B座",
+    classroomName: "B307",
+    weekDescription: "2-18周",
+    continuingSession: 3
+  }]
+}, {
+  _id: "4",
+  _openid: "10000100000",
+  className: "计算机组成原理实验",
+  teacherName: "罗川* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 4,
+    classSessions: 1,
+    campusName: "江安",
+    teachingBuildingName: "实验室",
+    classroomName: "二基楼B310B",
+    weekDescription: "11-15周",
+    continuingSession: 4
+  }]
+}, {
+  _id: "5",
+  _openid: "10000100000",
+  className: "微信移动终端开发",
+  teacherName: "方智阳* 网安外聘1 ",
+  classType: "任选",
+  timeAndPlaceList: [{
+    classDay: 5,
+    classSessions: 1,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A105",
+    weekDescription: "4-13周",
+    continuingSession: 4
+  }]
+}, {
+  _id: "6",
+  _openid: "10000100000",
+  className: "计算机新技术专题",
+  teacherName: "刘怡光* ",
+  classType: "任选",
+  timeAndPlaceList: [{
+    classDay: 1,
+    classSessions: 8,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A305",
+    weekDescription: "2-5周",
+    continuingSession: 2
   }, {
-      _id: "1",
-      _openid: "10000100000",
-      className: "马克思主义基本原理概论",
-      teacherName: "赵苏丹* 蒋永穆 ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 1,
-          classSessions: 3,
-          campusName: "江安",
-          teachingBuildingName: "综合楼C座",
-          classroomName: "C503",
-          weekDescription: "2-18周",
-          continuingSession: 2
-      }]
+    classDay: 4,
+    classSessions: 8,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A305",
+    weekDescription: "2-5周",
+    continuingSession: 2
+  }]
+}, {
+  _id: "7",
+  _openid: "10000100000",
+  className: "离散数学",
+  teacherName: "陈瑜* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 1,
+    classSessions: 1,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A506",
+    weekDescription: "2-18周",
+    continuingSession: 2
   }, {
-      _id: "2",
-      _openid: "10000100000",
-      className: "体育-3",
-      teacherName: "ty9* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 3,
-          classSessions: 8,
-          campusName: "江安",
-          teachingBuildingName: "体育场",
-          classroomName: "体育场2号",
-          weekDescription: "2-17周",
-          continuingSession: 2
-      }]
-  }, {
-      _id: "3",
-      _openid: "10000100000",
-      className: "中华文化（文学篇）",
-      teacherName: "谢谦* ",
-      classType: "任选",
-      timeAndPlaceList: [{
-          classDay: 1,
-          classSessions: 5,
-          campusName: "江安",
-          teachingBuildingName: "一教B座",
-          classroomName: "B307",
-          weekDescription: "2-18周",
-          continuingSession: 3
-      }]
-  }, {
-      _id: "4",
-      _openid: "10000100000",
-      className: "计算机组成原理实验",
-      teacherName: "罗川* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 4,
-          classSessions: 1,
-          campusName: "江安",
-          teachingBuildingName: "实验室",
-          classroomName: "二基楼B310B",
-          weekDescription: "11-15周",
-          continuingSession: 4
-      }]
-  }, {
-      _id: "5",
-      _openid: "10000100000",
-      className: "微信移动终端开发",
-      teacherName: "方智阳* 网安外聘1 ",
-      classType: "任选",
-      timeAndPlaceList: [{
-          classDay: 5,
-          classSessions: 1,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A105",
-          weekDescription: "4-13周",
-          continuingSession: 4
-      }]
-  }, {
-      _id: "6",
-      _openid: "10000100000",
-      className: "计算机新技术专题",
-      teacherName: "刘怡光* ",
-      classType: "任选",
-      timeAndPlaceList: [{
-          classDay: 1,
-          classSessions: 8,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A305",
-          weekDescription: "2-5周",
-          continuingSession: 2
-      }, {
-          classDay: 4,
-          classSessions: 8,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A305",
-          weekDescription: "2-5周",
-          continuingSession: 2
-      }]
-  }, {
-      _id: "7",
-      _openid: "10000100000",
-      className: "离散数学",
-      teacherName: "陈瑜* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 1,
-          classSessions: 1,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A506",
-          weekDescription: "2-18周",
-          continuingSession: 2
-      }, {
-          classDay: 3,
-          classSessions: 5,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A101",
-          weekDescription: "2-18周",
-          continuingSession: 3
-      }]
-  }, {
-      _id: "8",
-      _openid: "10000100000",
-      className: "计算机组成原理",
-      teacherName: "熊勇* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 2,
-          classSessions: 5,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A507",
-          weekDescription: "2-17周",
-          continuingSession: 3
-      }]
-  }, {
-      _id: "9",
-      _openid: "10000100000",
-      className: "数据结构与算法分析",
-      teacherName: "游洪跃* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 4,
-          classSessions: 5,
-          campusName: "江安",
-          teachingBuildingName: "一教A座",
-          classroomName: "A420",
-          weekDescription: "2-17周",
-          continuingSession: 3
-      }]
-  }, {
-      _id: "10",
-      _openid: "10000100000",
-      className: "数据结构与算法分析课程设计",
-      teacherName: "游洪跃* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 4,
-          classSessions: 8,
-          campusName: "江安",
-          teachingBuildingName: "实验室",
-          classroomName: "二基楼B301",
-          weekDescription: "6-15周",
-          continuingSession: 2
-      }]
-  }, {
-      _id: "11",
-      _openid: "10000100000",
-      className: "大学英语（创意阅读）-3",
-      teacherName: "许竞* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 3,
-          classSessions: 3,
-          campusName: "江安",
-          teachingBuildingName: "一教C座",
-          classroomName: "C203",
-          weekDescription: "2-18周",
-          continuingSession: 2
-      }]
-  }, {
-      _id: "12",
-      _openid: "10000100000",
-      className: "矩阵分析",
-      teacherName: "贺喆南* ",
-      classType: "必修",
-      timeAndPlaceList: [{
-          classDay: 2,
-          classSessions: 1,
-          campusName: "江安",
-          teachingBuildingName: "文科楼一区",
-          classroomName: "104",
-          weekDescription: "2-17周",
-          continuingSession: 3
-      }]
-  }
-]
+    classDay: 3,
+    classSessions: 5,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A101",
+    weekDescription: "2-18周",
+    continuingSession: 3
+  }]
+}, {
+  _id: "8",
+  _openid: "10000100000",
+  className: "计算机组成原理",
+  teacherName: "熊勇* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 2,
+    classSessions: 5,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A507",
+    weekDescription: "2-17周",
+    continuingSession: 3
+  }]
+}, {
+  _id: "9",
+  _openid: "10000100000",
+  className: "数据结构与算法分析",
+  teacherName: "游洪跃* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 4,
+    classSessions: 5,
+    campusName: "江安",
+    teachingBuildingName: "一教A座",
+    classroomName: "A420",
+    weekDescription: "2-17周",
+    continuingSession: 3
+  }]
+}, {
+  _id: "10",
+  _openid: "10000100000",
+  className: "数据结构与算法分析课程设计",
+  teacherName: "游洪跃* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 4,
+    classSessions: 8,
+    campusName: "江安",
+    teachingBuildingName: "实验室",
+    classroomName: "二基楼B301",
+    weekDescription: "6-15周",
+    continuingSession: 2
+  }]
+}, {
+  _id: "11",
+  _openid: "10000100000",
+  className: "大学英语（创意阅读）-3",
+  teacherName: "许竞* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 3,
+    classSessions: 3,
+    campusName: "江安",
+    teachingBuildingName: "一教C座",
+    classroomName: "C203",
+    weekDescription: "2-18周",
+    continuingSession: 2
+  }]
+}, {
+  _id: "12",
+  _openid: "10000100000",
+  className: "矩阵分析",
+  teacherName: "贺喆南* ",
+  classType: "必修",
+  timeAndPlaceList: [{
+    classDay: 2,
+    classSessions: 1,
+    campusName: "江安",
+    teachingBuildingName: "文科楼一区",
+    classroomName: "104",
+    weekDescription: "2-17周",
+    continuingSession: 3
+  }]
+}]
 
 const app = getApp()
 
@@ -227,11 +225,14 @@ Page({
     scrollLeft: 0,
 
     notes: [],
-    classSet: table,
+    classSet: [],
 
     onAddEvent: null,
     onCompleteEvent: null,
-    onDeleteEvent: null
+    onDeleteEvent: null,
+
+    onAddClass: null,
+    onDeleteClass: null
   },
 
   onLoad: function () {
@@ -259,13 +260,15 @@ Page({
       success: res => {
         const data = res.data
         this.setData({
-          // classSet: data
+          classSet: data
         })
       }
     })
     this.setData({
+      onAddEvent: this.onAddEvent,
+      onCompleteEvent: this.onCompleteEvent,
+      onDeleteEvent: this.onDeleteEvent,
       onAddClass: this.onAddClass,
-      onChangeClass: this.onChangeClass,
       onDeleteClass: this.onDeleteClass
     })
   },
@@ -288,7 +291,7 @@ Page({
           let notes = this.data.notes
           notes[0].events = events
           this.setData({
-            notes:notes
+            notes: notes
           })
         }
       })
@@ -347,12 +350,11 @@ Page({
         let notes = this.data.notes
         notes[k].events = events
         this.setData({
-          notes:notes
+          notes: notes
         })
       }
     })
   },
-  
   onDeleteEvent: function (noteID, eventID) {
     const db = wx.cloud.database()
     let events = []
@@ -376,7 +378,7 @@ Page({
           events: events
         },
         success: () => {
-          
+
           const notes = this.data.notes
           notes[pos].events = events
           this.setData({
@@ -387,103 +389,80 @@ Page({
     } else {
       db.collection("scheduleLine").doc(noteID).remove({
         success: () => {
-          
+
           const notes = []
           for (let i = 0; i < this.data.notes.length; i++) {
-            if(this.data.notes[i]._id === noteID) {
+            if (this.data.notes[i]._id === noteID) {
               continue
             }
             notes.push(this.data.notes[i])
           }
           this.setData({
-            notes:notes
+            notes: notes
           })
         }
       })
     }
-  }, 
+  },
 
-  onAddClass:function(event){
+  onAddClass: function (course) {
     const db = wx.cloud.database()
-    let table = this.data.classSet
-    for(let i = 0; i < table.length; k++) {
-      if (table[i]._id === _id) {
+    const table = this.data.classSet
+    let hasClass = false
+    for (let i = 0; i < table.length; i++) {
+      if (table[i].className == course.className) {
+        hasClass = true
+        let timeAndPlaceList = table[i].timeAndPlaceList
+        timeAndPlaceList.push(course.timeAndPlaceList[0])
+        console.log(timeAndPlaceList)
+        timeAndPlaceList[timeAndPlaceList.length - 1].classid = timeAndPlaceList[timeAndPlaceList.length - 2].classid + 1
+        db.collection("classTable").doc(table[i]._id).update({
+          data: {
+            timeAndPlaceList: timeAndPlaceList
+          },
+          success: () => {
+            table[i].timeAndPlaceList = timeAndPlaceList
+            this.setData({
+              classSet: table
+            })
+          }
+        })
         break
       }
-    } 
-    if (table[i].timeAndPlaceList.classid !== event.timeAndPlaceList.classid){
+    }
+    if (!hasClass) {
       db.collection('classTable').add({
-        data: {
-          className: event.className,
-          teacherName: event.teacherName,
-          classType: event.classType,
-          timeAndPlaceList:[{
-            classid: 0,
-            classDay:'',
-            classSessions:'',
-            campusName: event.campusName,
-            teachingBuildingName: event.teachingBuildingName,
-            classroomName: event.classroomName,
-            weekDescription: event.weekDescription,
-          }]
-        },
+        data: course,
         success: (res) => {
-          const table = [{
-            _id: res._id,
-            _openid: app.globalData.openid,
-            className: event.className,
-            teacherName: event.teacherName,
-            classType: event.classType,
-            timeAndPlaceList:[{
-              classid: 0,
-              classDay:'',
-              classSessions:'',
-              campusName: event.campusName,
-              teachingBuildingName: event.teachingBuildingName,
-              classroomName: event.classroomName,
-              weekDescription: event.weekDescription,
-            }]
-          }]
-          const classSet = table.concat(this.data.classSet)
+          course._id = res._id
+          course._openid = app.globalData.openid
+          table.push(course)
           this.setData({
-            classSet:classSet
+            classSet: table
           })
         }
       })
     }
   },
-
-  onChangeClass:function(_id, classid){
-    const db = wx.cloud.database
-    db.collection('todos').doc('todo-identifiant-aleatoire').update({
-      // data 传入需要局部更新的数据
-      data: {
-        
-      },
-      success: function(res) {
-        console.log(res.data)
-      }
-    })
-  },
-  onDeleteClass:function(_id, classid){
+  onDeleteClass: function (_id, classid) {
     const db = wx.cloud.database()
     let k
     let table = this.data.classSet
-    for(k = 0; k < table.length; k++) {
+    for (k = 0; k < table.length; k++) {
       if (table[k]._id === _id) {
         break
       }
-    } 
+    }
     if (table[k].timeAndPlaceList.length == 1) {
       db.collection("classTable").doc(_id).remove({
         success: () => {
           let newTable = []
-          for(let i = 0; i < table.length; i++) {
+          for (let i = 0; i < table.length; i++) {
             if (i == k) {
               continue
             }
             newTable.push(table[i])
-          } 
+          }
           this.setData({
             classSet: newTable
           })
@@ -492,17 +471,17 @@ Page({
     } else {
       let l
       let place = []
-      for (l = 0; l < table[k].timeAndPlaceList.length; l++){
+      for (l = 0; l < table[k].timeAndPlaceList.length; l++) {
         if (table[k].timeAndPlaceList[l].classid != classid) {
           place.push(table[k].timeAndPlaceList[l])
         }
       }
-      table[k].timeAndPlaceList = place
-      db.collection("").doc(_id).update({
+      db.collection("classTable").doc(_id).update({
         data: {
           timeAndPlaceList: place
         },
         success: () => {
+          table[k].timeAndPlaceList = place
           this.setData({
             classSet: table
           })
